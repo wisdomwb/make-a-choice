81b5c938c3312f0402459a9ac057c72cb1104d4e
weizhen <a610428@qq.com>
2018-09-08 09:11:11 +0800

    处理html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
@@ -12,7 +13,18 @@ module.exports = {
         loader: 'babel-loader',
       },
       exclude: /node_modules/
+    }, {
+      test: /\.html$/,
+      use: {
+        loader: 'html-loader'
+      }
     }]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
   mode: 'none'
 }
