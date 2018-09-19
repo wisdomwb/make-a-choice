29ae69a627b8738e5279226431519ef2ddcb8424
wz <wz@zksddeMac-mini.local>
2018-09-19 20:22:57 +0800

    添加split组件

--- src/pages/DivideProportion/index.js
+++ src/pages/DivideProportion/index.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 import * as style from './style.less'
 import { Tag, InputItem, Toast, Button, NavBar, Icon } from 'antd-mobile'
+import Split from 'split.js'
 
 class DivideProportion extends Component {
+
+  componentDidMount() {
+    Split(['#one', '#two', '#three'], {
+      minSize: 0,
+      elementStyle: function (dimension, size, gutterSize) {
+        return {
+          'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)'
+        }
+      },
+      gutterStyle: function (dimension, gutterSize) {
+        return {
+          'flex-basis': gutterSize + 'px'
+        }
+      }
+    })
+  }
 
   clickNext() {
 
@@ -24,7 +41,11 @@ class DivideProportion extends Component {
         <div className={style.divideProportion}>
           <div className={style.question}>问题：{question}</div>
           <div className={style.title}>划分这些因素的权重</div>
-
+          <div className={style.dividerWrapper}>
+            <div id="one">距离</div>
+            <div id="two">价格</div>
+            <div id="three">环境</div>
+          </div>
           <Button type='primary' onClick={this.clickNext.bind(this)} style={{ margin: '20px 0 0' }}>下一步</Button>
         </div>
       </div>
